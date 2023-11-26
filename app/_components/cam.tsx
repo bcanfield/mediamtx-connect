45465ebd927fb9eabf74a1d33ca9b591f5d10058
Brandin Canfield <12603953+bcanfield@users.noreply.github.com>
2023-11-25 22:16:50 -0500

    Working hls

--- app/_components/cam.tsx
+++ app/_components/cam.tsx
@@ -1,7 +1,28 @@
-// import appConfig from "@/lib/appConfig";
+"use client";
+import Hls from "hls.js";
+import { useEffect, useRef } from "react";
 
 export default function Cam({ props }: { props: { address: string } }) {
-  //   const camUrl = `${url}${hlsAddress}/${params.name}`;
-  console.log({ address: props.address });
-  return <iframe className="w-full h-full" src={props.address}></iframe>;
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+  useEffect(() => {
+    console.log({ address: props.address });
+    if (!videoRef.current || !props.address) {
+      return;
+    }
+
+    if (!Hls.isSupported()) {
+      videoRef.current.src = props.address;
+      return;
+    }
+
+    const hls = new Hls();
+    hls.loadSource(props.address);
+    hls.attachMedia(videoRef.current);
+
+    return () => {
+      console.log("hls destroy");
+      hls.destroy();
+    };
+  }, [props.address]);
+  return <video ref={videoRef} autoPlay controls />;
 }
