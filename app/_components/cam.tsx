e8ea949b1911622705de620e5cf618fe6b55a326
Brandin Canfield <12603953+bcanfield@users.noreply.github.com>
2023-11-25 22:38:09 -0500

    Hls improvement

--- app/_components/cam.tsx
+++ app/_components/cam.tsx
@@ -5,7 +5,38 @@ import { useEffect, useRef } from "react";
 export default function Cam({ props }: { props: { address: string } }) {
   const videoRef = useRef<HTMLVideoElement | null>(null);
   useEffect(() => {
-    console.log({ address: props.address });
+    const create = (video: HTMLVideoElement) => {
+      const hls = new Hls({
+        maxLiveSyncPlaybackRate: 1.5,
+      });
+
+      hls.on(Hls.Events.ERROR, (evt, data) => {
+        if (data.type === Hls.ErrorTypes.MEDIA_ERROR) {
+          console.log("recovering hls error");
+
+          hls.recoverMediaError();
+        } else if (data.fatal) {
+          console.log("fatal hls error");
+
+          hls.destroy();
+          setTimeout(() => create(video), 2000);
+        }
+      });
+
+      hls.on(Hls.Events.MEDIA_ATTACHED, () => {
+        console.log("media attached");
+        hls.loadSource(props.address);
+      });
+
+      hls.on(Hls.Events.MANIFEST_PARSED, () => {
+        console.log("manifest parsed");
+        video.play();
+      });
+
+      hls.attachMedia(video);
+      return hls;
+    };
+
     if (!videoRef.current || !props.address) {
       return;
     }
@@ -14,15 +45,11 @@ export default function Cam({ props }: { props: { address: string } }) {
       videoRef.current.src = props.address;
       return;
     }
-
-    const hls = new Hls();
-    hls.loadSource(props.address);
-    hls.attachMedia(videoRef.current);
-
+    const hls = create(videoRef.current);
     return () => {
       console.log("hls destroy");
-      hls.destroy();
+      hls?.destroy();
     };
   }, [props.address]);
-  return <video ref={videoRef} autoPlay controls />;
+  return <video ref={videoRef} muted={true} autoPlay controls />;
 }
