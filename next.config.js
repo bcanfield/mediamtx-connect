aa89536c56ab5db1d746c711fb2b4e3295dc172e
bcanfield <brandincanfield@gmail.com>
2023-11-26 14:53:33 -0500

    Initial cron

--- next.config.js
+++ next.config.js
@@ -3,12 +3,15 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   output: "standalone",
+  experimental: {
+    instrumentationHook: true,
+  },
   webpack: (config, { webpack }) => {
     // https://github.com/fluent-ffmpeg/node-fluent-ffmpeg/issues/573#issuecomment-1629414369
     config.plugins.push(
       new webpack.DefinePlugin({
         "process.env.FLUENTFFMPEG_COV": false,
-      })
+      }),
     );
     return config;
   },
