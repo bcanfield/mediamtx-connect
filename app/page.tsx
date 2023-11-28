4d53373b6e2fb6c8e496264c3d90843aa5c6ffef
CANFIELD.BRANDIN.W.1078680373.E <bcanfield@miletwo.us>
2023-11-28 12:07:10 -0500

    Error handling on instrumentation

--- app/page.tsx
+++ app/page.tsx
@@ -8,25 +8,33 @@ import StreamCard from "./_components/stream-card";
 export default async function Home() {
   const paths = await api.v3.pathsList();
   const mediaMtxConfig = await api.v3.configGlobalGet();
-  const { hlsAddress } = mediaMtxConfig.data;
+  const hlsAddress = mediaMtxConfig.error
+    ? undefined
+    : mediaMtxConfig.data.hlsAddress;
+
   const { remoteMediaMtxUrl } = appConfig;
 
-  const mtxItems = paths.data.items;
+  const mtxItems = mediaMtxConfig.error ? [] : paths.data.items;
+
   return (
     <PageLayout header="Online Cams">
-      <GridLayout columnLayout="small">
-        {mtxItems?.map(({ name, readyTime }, index) => (
-          <StreamCard
-            key={index}
-            props={{
-              streamName: name,
-              readyTime,
-              hlsAddress,
-              remoteMediaMtxUrl,
-            }}
-          ></StreamCard>
-        ))}
-      </GridLayout>
+      {hlsAddress ? (
+        <GridLayout columnLayout="small">
+          {mtxItems?.map(({ name, readyTime }, index) => (
+            <StreamCard
+              key={index}
+              props={{
+                streamName: name,
+                readyTime,
+                hlsAddress,
+                remoteMediaMtxUrl,
+              }}
+            ></StreamCard>
+          ))}
+        </GridLayout>
+      ) : (
+        <span>No HLS Address Configured for Live Viewing</span>
+      )}
     </PageLayout>
   );
 }
