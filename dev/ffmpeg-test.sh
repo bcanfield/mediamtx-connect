76c3ed53f7907e6c2de8ec0989944eea914b4acc
CANFIELD.BRANDIN.W.1078680373.E <bcanfield@miletwo.us>
2023-12-13 15:09:54 -0500

    Fixes

--- dev/ffmpeg-test.sh
+++ dev/ffmpeg-test.sh
@@ -8,7 +8,7 @@ fi
 
 # Run ffmpeg command multiple times based on STREAM_COUNT in the background
 for ((i = 1; i <= $1; i++)); do
-    ffmpeg -re -stream_loop -1 -i dev/test.mp4 -f rtsp -rtsp_transport tcp "rtsp://localhost:8554/test-stream$i" &
+    ffmpeg -re -stream_loop -1 -i dev/test.mp4 -c:v libx264 -preset ultrafast -tune zerolatency -c:a aac -strict -2 -f rtsp -rtsp_transport tcp "rtsp://localhost:8554/test-stream$i" &
 done
 
 # Wait for all background processes to complete
