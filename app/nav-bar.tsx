4a153f949aa0f20ce669e46b4ce0e2239e4d9e07
CANFIELD.BRANDIN.W.1078680373.E <bcanfield@miletwo.us>
2023-12-15 14:02:33 -0500

    Dropdown mobile nav

--- app/nav-bar.tsx
+++ app/nav-bar.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import { Menu } from "lucide-react";
+import { ChevronDown, Menu } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 type Props = {
   items?: { name: string; location: string }[];
@@ -22,8 +22,8 @@ export default function NavBar({ items }: Props) {
 
   return (
     <div className="flex h-16 items-center max-w-8xl space-x-4 sm:justify-between sm:space-x-0 mx-auto">
-      <div className="flex gap-6 sm:gap-10">
-        <Link href="/" className="hidden sm:flex items-center space-x-2">
+      <div className="flex gap-6 sm:gap-10 w-full">
+        <Link href="/" className="flex items-center space-x-2">
           <span className="font-bold inline-block">{"Connect"}</span>
         </Link>
         <>
@@ -41,34 +41,31 @@ export default function NavBar({ items }: Props) {
               </Link>
             ))}
           </nav>
-          <div className="flex sm:hidden items-center">
-            <Sheet>
-              <SheetTrigger>
-                <Menu className="w-6 h-6" />
-              </SheetTrigger>
-              <SheetContent side={"left"} className="flex flex-col gap-2">
-                <SheetHeader>
-                  <SheetTitle>
-                    <Link href="/" className="flex items-center space-x-2">
-                      <span className="font-bold inline-block">NextCams</span>
+          <div className="flex w-full sm:hidden items-center justify-end">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild className="flex items-center p-2">
+                <Button variant="ghost">
+                  <Menu></Menu>
+                  <ChevronDown className="w-4 h-4"></ChevronDown>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                {items?.map(({ location, name }, index) => (
+                  <DropdownMenuItem asChild key={index}>
+                    <Link
+                      key={location}
+                      href={location}
+                      className={cn(
+                        "text-muted-foreground transition-colors hover:text-primary",
+                        { "text-primary": pathname?.includes(location) },
+                      )}
+                    >
+                      {name}
                     </Link>
-                  </SheetTitle>
-                </SheetHeader>
-                {items?.map(({ location, name }) => (
-                  <Link
-                    key={location}
-                    href={location}
-                    className={cn(
-                      "text-muted-foreground transition-colors hover:text-primary",
-                      { "text-primary": pathname?.includes(location) },
-                    )}
-                  >
-                    {name}
-                  </Link>
+                  </DropdownMenuItem>
                 ))}
-                {/* {links} */}
-              </SheetContent>
-            </Sheet>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         </>
       </div>
