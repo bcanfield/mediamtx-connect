78ef7944d1c22f566ee29b331fc350a6ced5b786
Brandin Canfield <12603953+bcanfield@users.noreply.github.com>
2023-12-17 00:07:01 -0500

    View recording on recording page

--- app/recordings/[streamname]/page.tsx
+++ app/recordings/[streamname]/page.tsx
@@ -7,29 +7,13 @@ import getRecordings, {
 } from "@/app/_actions/getStreamRecordings";
 import GridLayout from "@/app/_components/grid-layout";
 import PageLayout from "@/app/_components/page-layout";
+import RecordingCard from "@/app/_components/recording-card";
 import { getFilesInDirectory } from "@/app/utils/file-operations";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-} from "@/components/ui/card";
-import dayjs from "dayjs";
-import {
-  AlertTriangle,
-  ChevronLeft,
-  ChevronRight,
-  Image as ImageIcon,
-  Play,
-} from "lucide-react";
+import { AlertTriangle, ChevronLeft, ChevronRight } from "lucide-react";
 import { Url } from "next/dist/shared/lib/router/router";
-import Image from "next/image";
 import Link from "next/link";
 import path from "path";
-import DownloadVideo from "./_components/downloadVideo";
 export default async function Recordings({
   params,
   searchParams,
@@ -109,45 +93,15 @@ export default async function Recordings({
             streamName: params.streamname,
           });
           return (
-            <Card key={name} className="flex flex-col">
-              <CardHeader>
-                <CardDescription className="text-xs">
-                  <span>
-                    {dayjs(createdAt).format("dddd, MMMM D, YYYY h:mm A")}
-                  </span>
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="flex flex-col gap-2 ">
-                <div className="min-h-[14rem] flex items-center">
-                  {base64 ? (
-                    <Image
-                      // className="flex-auto"
-                      width={640}
-                      height={480}
-                      alt=""
-                      src={base64}
-                    ></Image>
-                  ) : (
-                    <div className=" flex-auto flex items-center justify-center">
-                      <ImageIcon></ImageIcon>
-                    </div>
-                  )}
-                </div>
-              </CardContent>
-              <CardFooter className="flex gap-2">
-                <div className="flex-1">
-                  <DownloadVideo
-                    streamName={params.streamname}
-                    filePath={name}
-                  ></DownloadVideo>
-                </div>
-                <Link className="flex-1" href={`${params.streamname}/${name}`}>
-                  <Button className="w-full" variant="outline">
-                    <Play className="w-4 h-4"></Play>
-                  </Button>
-                </Link>
-              </CardFooter>
-            </Card>
+            <RecordingCard
+              key={name}
+              props={{
+                thumbnail: base64,
+                createdAt: createdAt,
+                fileName: name,
+                streamName: params.streamname,
+              }}
+            ></RecordingCard>
           );
         })}
       </GridLayout>
